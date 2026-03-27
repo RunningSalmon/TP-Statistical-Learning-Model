4e37d91e0c3973d6bfbfb966c6885471e9dfcfc0
RunningSalmon <lhgl@web.de>
2026-03-27 14:42:13 +0100

    implemented the calculation of the transitional probabilities from syllable streams

---
+++ StatisticalTraining.py
@@ -0,0 +1,27 @@
+
+
+def find_TP(speech_stream: list[str]) -> dict:
+    """calculates the probability of syllable a being followed by syllable b
+    :return a dict in which the probability of a being followed by b is denoted in dict[(a,b)]"""
+    #find quantity of syllables as well as transitional quantity
+    unigram_counts = {}
+    bigram_counts = {}
+    for i in range(len(speech_stream)-1):
+        #count general quantity
+        syllable = speech_stream[i]
+        if syllable not in unigram_counts:
+            unigram_counts[syllable] = 0
+        unigram_counts[syllable] += 1
+
+        #count transitions
+        next_syllable = speech_stream[i+1]
+        if (syllable, next_syllable) not in bigram_counts:
+            bigram_counts[syllable, next_syllable] = 0
+        bigram_counts[syllable, next_syllable] += 1
+
+    TPs = {}
+
+    for (a,b), count in bigram_counts.items():
+        TPs[(a,b)] = count/unigram_counts[a]
+
+    return TPs
